a03d59007c484f1968c7ae8b0d545e933b203eb7
Yoshimitsu Torii <yoshimitsu.torii@gmail.com>
2014-11-30 12:48:45 +0900

    add companies data delete initialize in index.html

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,19 @@ var resourceBundle = require('./modules/resourceBundle');
 var config = require('../config').config;
 var router = express.Router();
 
+var companies = require('../data/companies');
+
+console.log(companies);
+
 /* GET home page. */
 router.get('/', function(req, res) {
     var labels = resourceBundle.getLabels(req);
+
     res.render('index', {
         title: labels.main_title,
         config: config,
-        labels: labels
+        labels: labels,
+        data: JSON.stringify(companies)
     });
 });
 
