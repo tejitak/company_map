75c890f17fc5194341729c4d40182aa99aac076f
Kyota Masuyama <bmm.jt.g.1adv@gmail.com>
2015-04-26 11:08:35 +0900

    make statmap.js which is sample of register form

--- public/js/teji/company/components/add-company.vue
+++ public/js/teji/company/components/add-company.vue
@@ -2,10 +2,28 @@
 
   <section class="addcompany-content modal-content">
     <i class="modal-overlay" v-on="click: close()"></i>
-    
+
     <div class="modal-body pahse-start">
       <header class="modal-header">
         <h1 class="heading-1">会社の登録</h1>
+          <div class="form__content">
+            <dl class="input-line">
+              <dt class="label">URL</dt>
+              <dd class="form-asset"><input type="text" class="input-text" value="" id="com_url" /></dd>
+            </dl>
+            <dl class="input-line">
+              <dt class="label">住所</dt>
+              <dd class="form-asset"><input type="text" class="input-text" value="" id="com_address" /></dd>
+            </dl>
+          </div>
+          <div class="settelments">
+            <button class="submit ok" v-on="click preview()">追加する</button>
+            <button class="submit cancel">キャンセル</button>
+            <div id="statmap"><img /></div>
+          </div>
+      <div class="closeWrap">
+        <a href="javascript:;" v-on="click: close()" class="close" title="Close"></a>
+      </div>
         <div class="closeWrap functions">
           <div class="route">
             <i class="circle on"></i>
@@ -24,10 +42,10 @@
         <button class="submit ok">OK</button>
       </footer>
     </div>
-    
+
     <!-- modal-body phase-confirm -->
     <!-- modal-body phase-end -->
-    
+
     <div class="modal-error">
       <p class="error-text">おっと、URLが正しい形式ではないようです…</p>
       <p class="error-text">おっと、住所が未記入ですよ…</p>
